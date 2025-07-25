1bac616bb95c0aab7c2cd2f04ed65293779eb5b3
KKamjangE <ajm980718@gmail.com>
2025-07-25 14:31:28 +0900

    이메일 인증번호 확인 api 생성

--- src/modules/auth/server/api.ts
+++ src/modules/auth/server/api.ts
@@ -1,7 +1,23 @@
 import { customFetch } from "@/lib/custom-fetch";
 
-export const postEmailVerify = async (data: { email: string }) => {
+type emailVerifyReq = {
+  email: string;
+};
+
+export const postEmailVerify = async (data: emailVerifyReq) => {
   return await customFetch("/users/email/verify", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+};
+
+type VerifyCheckReq = {
+  email: string;
+  code: string;
+};
+
+export const postVerifyCheck = async (data: VerifyCheckReq) => {
+  return await customFetch("/users/email/verify/check", {
     method: "POST",
     body: JSON.stringify(data),
   });
--- src/modules/auth/server/mutations.ts
+++ src/modules/auth/server/mutations.ts
@@ -1,6 +1,10 @@
-import { postEmailVerify } from "@/modules/auth/server/api";
+import { postEmailVerify, postVerifyCheck } from "@/modules/auth/server/api";
 import { mutationOptions } from "@tanstack/react-query";
 
 export const emailVerifyOptions = mutationOptions({
   mutationFn: postEmailVerify,
+});
+
+export const verifyCheckOptions = mutationOptions({
+  mutationFn: postVerifyCheck,
 });
