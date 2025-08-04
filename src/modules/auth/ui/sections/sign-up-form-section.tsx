5b074193a585e550fb0cb51c306528e7413393a4
KKamjangE <ajm980718@gmail.com>
2025-08-04 16:10:47 +0900

    비밀번호, 닉네임 schema 정규표현식 추가

--- src/modules/auth/ui/sections/sign-up-form-section.tsx
+++ src/modules/auth/ui/sections/sign-up-form-section.tsx
@@ -5,6 +5,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -71,10 +72,20 @@ type SignUpStep = {
 
 const signUpSchema = z.object({
   email: z.email({ error: '이메일을 입력해주세요.' }),
-  password: z.string().pipe(notEmpty('비밀번호를 입력해주세요.')),
+  password: z
+    .string()
+    .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
+    .max(20, '비밀번호는 최대 20자까지 허용됩니다.')
+    .regex(/[a-zA-z]/, '영문을 포함해주세요.')
+    .regex(/\d/, '숫자을 포함해주세요.')
+    .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, '특수문자을 포함해주세요.')
+    .pipe(notEmpty('비밀번호를 입력해주세요.')),
   confirmPassword: z.string().pipe(notEmpty('비밀번호를 입력해주세요.')),
   code: z.string().pipe(notEmpty('인증번호를 입력해주세요.')),
-  nickname: z.string().pipe(notEmpty('이름을 입력해주세요.')),
+  nickname: z
+    .string()
+    .max(20, '이름은 최대 20자까지 허용됩니다.')
+    .pipe(notEmpty('이름을 입력해주세요.')),
   job: z.string().pipe(notEmpty('직무를 선택해주세요.')),
   position: z.string().pipe(notEmpty('직급을 선택해주세요.')),
   joinYear: z.string().pipe(notEmpty('입사년도를 선택해주세요.')),
@@ -207,6 +218,7 @@ const SignUp = ({ onNext }: SignUpProps) => {
               <Input {...field} type="password" />
             </FormControl>
             <FormMessage />
+            <FormDescription>영문, 숫자, 문자를 포함한 8~20자</FormDescription>
           </FormItem>
         )}
       />
