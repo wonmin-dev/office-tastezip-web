609996a2f09d294faec75fca0e1762d2d5918566
KKamjangE <ajm980718@gmail.com>
2025-08-05 16:11:52 +0900

    비밀번호 암호화 유틸 함수 생성

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import z from "zod";
+import JSEncrypt from "jsencrypt";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,3 +9,15 @@ export function cn(...inputs: ClassValue[]) {
 
 export const notEmpty = (message = "Required") =>
   z.string().trim().min(1, { message });
+
+export const passwordRsaEncrypt = (password: string, rsaKey?: string) => {
+  if (!rsaKey) throw new Error("암호화에 실패했습니다.");
+
+  const encrypt = new JSEncrypt();
+  encrypt.setPublicKey(rsaKey);
+  const encrypted = encrypt.encrypt(password);
+
+  if (encrypted === false) throw new Error("암호화에 실패했습니다.");
+
+  return encrypted;
+};
