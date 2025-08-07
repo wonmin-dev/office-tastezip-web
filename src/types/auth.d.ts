7bd42bf7bd89185b7ef34f9515b88341c8718563
KKamjangE <ajm980718@gmail.com>
2025-08-07 17:03:20 +0900

    next-auth 타입 확장

---
+++ src/types/auth.d.ts
@@ -0,0 +1,17 @@
+import type { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+  }
+
+  interface User {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
