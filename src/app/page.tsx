ac7200f867856d50e9dc30ceb8551b0d4b36eecc
KKamjangE <ajm980718@gmail.com>
2025-08-07 17:02:44 +0900

    임시 로그아웃 버튼 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { SignOutButton } from '@/app/sign-out-button';
+import { auth, signOut } from '@/auth';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  console.log(session);
+
+  const logOut = () => {
+    signOut();
+  };
+
   return (
     <div>
       <Link href={'/sign-in'}>로그인페이지</Link>
+      <SignOutButton />
     </div>
   );
 }
---
+++ src/app/sign-out-button.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { signOut } from 'next-auth/react';
+
+export const SignOutButton = () => {
+  return <Button onClick={() => signOut()}>SignOut</Button>;
+};
